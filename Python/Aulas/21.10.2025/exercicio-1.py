848b48d62680b7cbd49bb348644b83c6478e712e
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-21 19:31:46 -0300

    Adição do arquivo

--- Python/Aulas/21.10.2025/exercicio-1.py
+++ Python/Aulas/21.10.2025/exercicio-1.py
@@ -1,7 +1,10 @@
 lista = []
 
-while len(lista) < 9:
-    valor = input(f"({10-len(lista)} RESTANTES) Informe um valor: ")
-    lista.append(valor)
+def Adicionar10ValoresNaLista(lista):
+    while len(lista) < 9:
+        valor = input(f"({10-len(lista)} RESTANTES) Informe um valor: ")
+        lista.append(valor)
+
+Adicionar10ValoresNaLista(lista)
 
 print(lista)
